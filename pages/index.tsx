8251e06a119831deace535f85777b82cc7c36e4c
sarneijim <38540290+sarneijim@users.noreply.github.com>
2023-05-15 14:45:03 +0100

    feat: my first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
+    <button>Execute</button>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
