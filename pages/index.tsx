ad1545a0b7bbf529bce74c5d8e05c418a4395bec
sarneijim <38540290+sarneijim@users.noreply.github.com>
2023-05-15 19:40:59 +0100

    fix: support params from LL

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import { useCallback } from "react";
 import Layout from "../components/Layout";
+import { useSearchParams } from "next/navigation";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <button>Execute</button>
-  </Layout>
-);
+const IndexPage = (props) => {
+  const searchParams = useSearchParams();
+  const provider = searchParams.get("provider");
+  const onSwap = useCallback(() => {
+    console.log("provider", provider);
+  }, [provider]);
+
+  return (
+    <Layout title="Swao Web App Example">
+      <h1>Hello I am a Swap Web app</h1>
+      <button onClick={() => onSwap()}>{"Execute swap"}</button>
+    </Layout>
+  );
+};
 
 export default IndexPage;
