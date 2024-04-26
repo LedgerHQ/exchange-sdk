a35dc334f2e60a223cf4e04cf3b242e2bff6d427
Thomas Brillard <thomas.brillard@ledger.fr>
2024-04-26 16:35:47 +0200

    chore: update on error management

--- lib/src/handleErrors.ts
+++ lib/src/handleErrors.ts
@@ -14,6 +14,7 @@ export function handleErrors(walletAPI: WalletAPIClient<any>, error: any) {
       "WrongDeviceForAccountPayout",
       "WrongDeviceForAccountRefund",
       "CancelStepError",
+      "ConfirmStepError",
       "SwapCompleteExchangeError",
   ]);
 
